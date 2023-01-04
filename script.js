198b348ba10c6a8b840fb19718adac008c2d27f0
Devin McDaniel <dmcdaniel9@gmail.com>
2023-01-04 17:23:46 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//^ DESCRIBE
+// Create a simple digital clock. The clock can be in 12 or 24hr format (or both!)
+// and should also display the day of the week. Contain the clock inside of a centered module
+// on the webpage.
+
+//^ QUESTIONS
+
+//^ How do I get the current time?
+/*  const date = new Date
+    console.log(`The time is ${date.getHours()}:${date.getMinutes()}`)
+*/
+
+//^ How do I split the time into digits?
+/*const timeArray = Array.from(date.getHours().toString() + date.getMinutes().toString())
+
+console.log(timeArray)
+*/
+
+
+
+//^ (Opt) How do I implement a choice between 12 and 24hr time?
+
+
+//^ How do I get the day of the week?
+/* const date = new Date;
+   date.currentDay = function() {
+    const today = this.getDay()
+
+  switch(today) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    }
+  }
+
+
+const today = date.currentDay()
+console.log(today)
+
+console.log(`The time is ${date.getHours()}:${date.getMinutes()}`)
+*/
